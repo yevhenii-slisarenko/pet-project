51711096e66e27dca8c20809a0915da9e880d344
yevhenii-slisarenko <brat.kotekita9@gmail.com>
2025-08-19 16:57:38 +0300

    added corsConfigurationSource to config

--- backend/my-project/src/main/java/my/iam_service/config/SecurityConfig.java
+++ backend/my-project/src/main/java/my/iam_service/config/SecurityConfig.java
@@ -29,6 +29,7 @@ import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.security.config.Customizer;
+import java.util.List;
 
 @Configuration
 @EnableWebSecurity
