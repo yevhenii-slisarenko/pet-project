c1c09fd3fed073c7b91ce4b41c7fb1cf91ab32cb
yevhenii-slisarenko <brat.kotekita9@gmail.com>
2025-08-19 14:49:45 +0300

    add cors

--- backend/my-project/src/main/java/my/iam_service/config/CorsConfig.java
+++ backend/my-project/src/main/java/my/iam_service/config/CorsConfig.java
@@ -14,7 +14,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:3000")
+                        .allowedOrigins("http://https://my-pet-project-iamservice-forum.netlify.app")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
